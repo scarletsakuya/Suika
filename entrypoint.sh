dbf7e8742a904943ebb5ad908b4ccaa41b68a930
scarletsakuya <31232410+scarletsakuya@users.noreply.github.com>
2019-06-19 14:46:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-echo ${ROOT_PASSWORD}
-date
-
-echo root:${ROOT_PASSWORD} | chpasswd
-mkdir -p -m 0755 /var/run/sshd
-
+date -R
 echo "Start Success !"
 
-sed -i "s/\[VMESS_PORT\]/${VMESS_PORT}/g" /etc/v2ray/config.json
 sed -i "s/\[VMESS_ID\]/${VMESS_ID}/g" /etc/v2ray/config.json
 sed -i "s/\[VMESS_LEVEL\]/${VMESS_LEVEL}/g" /etc/v2ray/config.json
 sed -i "s/\[VMESS_ALTERID\]/${VMESS_ALTERID}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_PORT_VMESS\]/${KCP_PORT_VMESS}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_MUT\]/${KCP_MUT}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_TTI\]/${KCP_TTI}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_UPLINK\]/${KCP_UPLINK}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_DOWNLINK\]/${KCP_DOWNLINK}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_READBUFF\]/${KCP_READBUFF}/g" /etc/v2ray/config.json
-sed -i "s/\[KCP_WRITEBUFF\]/${KCP_WRITEBUFF}/g" /etc/v2ray/config.json
-sed -i "s/\[TOKEN]/${TOKEN}/g" /root/.ehforwarderbot/profiles/default/blueset.telegram/config.yaml
-sed -i "s/\[ADM]/${ADM}/g" /root/.ehforwarderbot/profiles/default/blueset.telegram/config.yaml
 
 for i in "$@"; do
 	bash $i
 done
 
 cat /etc/v2ray/config.json
-
-(/usr/sbin/sshd -D -e ) &
 
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json
